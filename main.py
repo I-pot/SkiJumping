297bb98ad15f7ac5579b3a14065a21916810e8d9
Topi <topi.lowalt@gmail.com>
2023-03-09 10:52:58 +0200

    First commit. Creating repository and adding main.py.

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+#from kivy.core.text_input import TextInput
+
+print("Hello World!")
+
