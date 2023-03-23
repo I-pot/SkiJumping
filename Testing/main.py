662b99dee6f1fbf8dfca1b1bdb4bcc0976037793
Topi <topi.lowalt@gmail.com>
2023-03-23 17:54:06 +0200

    Going through the Kivy-tutorials next.

---
+++ Testing/main.py
@@ -0,0 +1,17 @@
+#Testing 
+#Taken from https://kivy.org/doc/stable/guide/basic.html
+import kivy
+kivy.require('2.1.0') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
